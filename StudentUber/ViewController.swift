91058c27fe0146e9a6025e3d1a71796fe82cea52
Chiraag Nadig <chigu.nadig@gmail.com>
2022-06-12 17:29:01 -0400

    Initial Commit

---
+++ StudentUber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudentUber
+//
+//  Created by Chiraag Nadig on 6/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
